b854c488c33bc2497fcee1571032e493e36f964f
Rod Ritter <rod@ritter.co.za>
2022-09-27 10:35:10 +0200

    breakpoint fix

--- src/lib/breakpoints.js
+++ src/lib/breakpoints.js
@@ -1,6 +1,6 @@
 const breakpoints = {
-    lg: "@media only screen and (max-width: 1500px)",
-    md: "@media only screen and (max-width: 960px)",
+    lg: "@media only screen and (max-width: 960px)",
+    md: "@media only screen and (max-width: 700px)",
     sm: "@media only screen and (max-width: 500px)",
 };
 
